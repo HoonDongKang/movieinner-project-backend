59735e8247e435f4ae2eefe888e52b94692c3967
Donghoon Kang <d159123@naver.com>
2022-08-06 20:33:46 +0900

    siginin

--- src/apis/signin.ts
+++ src/apis/signin.ts
@@ -1,10 +1,19 @@
 import express from 'express'
-
+import mysql from 'mysql2/promise'
+import DB from '../configs/db'
 export const signInRouter = express.Router()
+const { DB_NAME, HOST, ID, PASSWORD } = DB
+const DATABASE_URL = `
+    mysql://${ID}:${PASSWORD}@${HOST}/${DB_NAME}?ssl={"rejectUnauthorized":true}`
 
-signInRouter.get('/', async (req, res) => {
-    res.json({
-        connection: true,
-        message: 'Connection Success',
-    })
+signInRouter.post('/', async (req, res) => {
+    const { id, password } = req.body
+    const connection = await mysql.createConnection(DATABASE_URL)
+    const [[rows], fields] = await connection.execute(
+        `SELECT COUNT(*) AS count FROM user_info WHERE id=? AND password=?`,
+        [id, password]
+    )
+    const { count } = rows
+    if (count > 0) {
+    }
 })
