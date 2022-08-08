11b4d9a75c46723cbe6cc0d55c190f40967a3cef
Donghoon Kang <d159123@naver.com>
2022-08-08 12:12:42 +0900

    testApi

--- src/apis/testAPI.ts
+++ src/apis/testAPI.ts
@@ -1,16 +1,4 @@
-import express from 'express'
-import { Request } from 'express'
-import { ReqConnection } from '../middlewares/dbConnect'
 import { DbConnection } from '../modules/connect'
-
-// export const router = express.Router()
-
-// router.get('/', async (req: Request, res) => {
-//     const request = req as ReqConnection
-//     const connection = request.mysqlConnection
-//     const response = await connection.run(`SELECT * FROM user_info`, [])
-//     res.json({ response: response })
-// })
 
 const testAPI = async (params: any, connection: DbConnection) => {
     return {
