b26e497707e1ade61576430d5164af25dd3e0b82
HoonDongKang <d159123@naver.com>
2022-08-16 20:09:27 +0900

    USER API

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,6 +1,7 @@
 // 유저 정보 CRUD API
 import { DbConnection } from './../modules/connect'
 
+//모든 유저 이메일, 이름 가져오기
 const getUsersInfo = async (params: any, connection: DbConnection) => {
     const response = await connection.run(
         `SELECT email,name FROM user_info`,
@@ -9,6 +10,7 @@ const getUsersInfo = async (params: any, connection: DbConnection) => {
     return { status: 200, data: response }
 }
 
+//특정 유저의 정보 가져오기
 const getIdxUserInfo = async (params: any, connection: DbConnection) => {
     const { insertId } = params
     const response = await connection.run(
@@ -16,6 +18,11 @@ const getIdxUserInfo = async (params: any, connection: DbConnection) => {
         [insertId]
     )
     return { status: 200, data: response }
+}
+
+//모든 유저 정보 삭제
+const deleteUsers = async (params: any, connection: DbConnection) => {
+    await connection.run(`DELETE FROM `)
 }
 
 export default {
