7e692b46584947ebbb2f33bfdd279ed30dfc2d98
HoonDongKang <d159123@naver.com>
2022-08-17 12:47:39 +0900

    s3 config

--- src/configs/s3.ts
+++ src/configs/s3.ts
@@ -4,4 +4,5 @@ dotenv.config()
 export default {
     AWS_S3_ACCESS_ID: process.env.AWS_S3_ACCESS_ID,
     AWS_S3_ACCESS_KEY: process.env.AWS_S3_ACCESS_KEY,
+    AWS_S3_REGION: process.env.AWS_S3_REGION,
 }
