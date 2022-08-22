6631768bba50c9927e5c2c5d614726aa9d21be06
HoonDongKang <d159123@naver.com>
2022-08-22 16:36:31 +0900

    error handler

--- src/apis/signin.ts
+++ src/apis/signin.ts
@@ -8,8 +8,14 @@ const signin = async (params: any, connection: DbConnection) => {
         `SELECT password FROM user_info WHERE email=? AND idx=?`,
         [email, insertId]
     )
+    if (!response) {
+        throw new Error('E0001')
+    }
     const { password: hashedpassword } = response[0]
     const isEqual = await bcrypt.compare(password, hashedpassword)
+    if (!isEqual) {
+        throw new Error('E0003')
+    }
     return { status: 200, data: { success: isEqual } }
 }
 
