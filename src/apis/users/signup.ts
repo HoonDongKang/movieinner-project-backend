cdb3e02ce54639cf5b61b2b8169ec28e275ac18d
HoonDongKang <d159123@naver.com>
2022-08-23 00:00:59 +0900

    signup

--- src/apis/users/signup.ts
+++ src/apis/users/signup.ts
@@ -5,12 +5,13 @@ import { DbConnection } from '../../modules/connect'
 //회원가입
 const signup = async (params: any, connection: DbConnection) => {
     try {
-        const { email, password, name, gender, birth, nickname } = params
+        const { email, password, image_URL, name, gender, birth, nickname } =
+            params
         const salt = await bcrypt.genSalt(10)
         const hashedPw = await bcrypt.hash(password, salt)
         await connection.run(
-            `INSERT INTO user_info(email,password,name,gender,birth,nickname) VALUES(?,?,?,?,?,?)`,
-            [email, hashedPw, name, gender, birth, nickname]
+            `INSERT INTO user_info(email,password,image_URL,name,gender,birth,nickname) VALUES(?,?,?,?,?,?,?)`,
+            [email, hashedPw, image_URL, name, gender, birth, nickname]
         )
         return {
             status: 201,
