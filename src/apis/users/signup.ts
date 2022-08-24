15a72315289b5b1a604ced80306b096ab0b5552e
HoonDongKang <d159123@naver.com>
2022-08-25 00:35:45 +0900

    signup

--- src/apis/users/signup.ts
+++ src/apis/users/signup.ts
@@ -3,11 +3,23 @@ import bcrypt from 'bcrypt'
 import { DbConnection } from '../../modules/connect'
 
 //회원가입
-const signup = async (params: any, connection: DbConnection) => {
+
+interface SignupParamsType {
+    email: string
+    password: string
+    image_URL: string
+    name: string
+    gender: string
+    birth: string
+    nickname: string
+}
+
+const signup = async (params: SignupParamsType, connection: DbConnection) => {
     try {
         const { email, password, image_URL, name, gender, birth, nickname } =
             params
         const salt = await bcrypt.genSalt(10)
+        if (!password) throw new Error('password missing')
         const hashedPw = await bcrypt.hash(password, salt)
         await connection.run(
             `INSERT INTO user_info(email,password,image_URL,name,gender,birth,nickname) VALUES(?,?,?,?,?,?,?)`,
@@ -18,7 +30,14 @@ const signup = async (params: any, connection: DbConnection) => {
             data: { success: true },
         }
     } catch (e: any) {
-        throw new Error(e)
+        if (
+            e.message ===
+            'Bind parameters must not contain undefined. To pass SQL NULL specify JS null'
+        ) {
+            throw new Error('E0001')
+        } else {
+            throw new Error(e)
+        }
     }
 }
 
