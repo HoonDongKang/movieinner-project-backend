334f6b245cffb28ef8017462f9a70b00bb737d0f
HoonDongKang <d159123@naver.com>
2022-08-29 16:14:20 +0900

    google

--- src/configs/kakao.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-export default {
-    KAKAO_REST_API_KEY: process.env.KAKAO_REST_API_KEY as string,
-    KAKAO_REDIRECT_URI: process.env.KAKAO_REDIRECT_URI as string,
-}
