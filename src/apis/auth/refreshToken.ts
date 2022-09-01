76a3ebca2cea8aac17312c046fc149448f312dfd
HoonDongKang <d159123@naver.com>
2022-09-01 15:23:56 +0900

    refresh token

--- src/apis/auth/refreshToken.ts
+++ src/apis/auth/refreshToken.ts
@@ -5,8 +5,14 @@ import { jwtErrorHandler } from "../../modules/paramsError";
 const { JWT_SECRET } = jsonWebToken
 
 const refreshToken = async (params: any, connection: DbConnection) => {
+    let newAccessTokenPayload={}
     let newAccessToken=''
+    let newRefreshTokenPayload={}
     let newRefreshToken=''
+    const expiredDate = new Date(Date.now() + 3600 * 1000 * 24) //24시간
+    const refreshTokenExpiredDate = new Date(
+        Date.now() + 3600 * 1000 * 24 * 180
+    )//6개월
     try{
         const {accessToken, refreshToken} = params
         const accessTokehPayload:any = JWT.verify(accessToken,JWT_SECRET)
@@ -14,17 +20,24 @@ const refreshToken = async (params: any, connection: DbConnection) => {
         const refreshTokenPayload:any = JWT.verify(refreshToken,JWT_SECRET)
         const now = Date.now()
         
-        const getResponse=await connection.run(`SELECT access_token,expires_in, refresh_token_expires_in FROM user_token WHERE refresh_token=?`,[refreshToken])
+        const getResponse=await connection.run(`SELECT refresh_token_expires_in FROM user_token WHERE refresh_token=?`,[refreshToken])
         //refresh token이 db에 존재할 경우
         if(getResponse[0]){
             //refresh token의 access token이 parameter로 온 access token과 같을 경우
             if(refreshTokenPayload.accessToken===accessToken){
-                const {access_token,expires_in,refresh_token_expires_in}=getResponse[0]
+                const {refresh_token_expires_in}=getResponse[0]
                 const refresh_token_expires_in_number= Date.parse(refresh_token_expires_in)
                 // refresh token의 만료가 1개월 미만일 경우
                 if(refresh_token_expires_in_number>now+3600 * 1000 * 24 * 30){
-                    const newAccessTokenPayload={email,idx,nickname,now}
-                    
+                     newAccessTokenPayload={email,idx,nickname,expiredDate}
+                     newAccessToken=JWT.sign(newAccessTokenPayload,JWT_SECRET)
+                     newRefreshTokenPayload={newAccessToken,refreshTokenExpiredDate}
+                     newRefreshToken=JWT.sign(newRefreshTokenPayload,JWT_SECRET)
+                }else{ //1개월 이상일 경우 기존 refresh token의 만료기간을 유지
+                    newAccessTokenPayload={email,idx,nickname,expiredDate}
+                    newAccessToken=JWT.sign(newAccessTokenPayload,JWT_SECRET)
+                    newRefreshTokenPayload={newAccessToken,refresh_token_expires_in}
+                    newRefreshToken=JWT.sign(newRefreshTokenPayload,JWT_SECRET)
                 }
             }else{
                 throw 'E0005'
