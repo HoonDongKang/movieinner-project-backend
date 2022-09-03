bdbec8e1abd20376f41bc35732c541a0e23c7008
HoonDongKang <d159123@naver.com>
2022-09-03 22:25:12 +0900

    delete insertID

--- src/apis/users/signin.ts
+++ src/apis/users/signin.ts
@@ -4,15 +4,15 @@ import { DbConnection } from '../../modules/connect'
 import { paramsErrorHandler } from '../../modules/paramsError'
 
 const signin = async (
-    params: { email: string; insertId: string; password: string },
+    params: { email: string; password: string },
     connection: DbConnection
 ) => {
     let isEqual = false
     try {
-        const { email, insertId, password } = params //idx값을 줘야하나?
+        const { email, password } = params //idx값을 줘야하나?
         const response = await connection.run(
-            `SELECT password FROM user_info WHERE email=? AND idx=?`,
-            [email, insertId]
+            `SELECT password FROM user_info WHERE email=?`,
+            [email]
         )
         if (!response[0]) {
             throw 'E0006'
