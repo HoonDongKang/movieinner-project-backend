d195a145f159f1e576a271e0694a6aba7081b0a4
HoonDongKang <d159123@naver.com>
2022-09-05 10:25:07 +0900

    authorizer

--- src/middlewares/authorizer.ts
+++ src/middlewares/authorizer.ts
@@ -1,38 +1,39 @@
-import { NextFunction, Response } from 'express'
-import { ReqConnectionUndefined } from './dbConnect'
-import JWT from 'jsonwebtoken'
-import jsonWebToken from '../configs/jsonWebToken'
-const { JWT_SECRET } = jsonWebToken
+import { NextFunction, Response } from "express";
+import { ReqConnectionUndefined, ReqConnection } from "./dbConnect";
+import JWT from "jsonwebtoken";
+import jsonWebToken from "../configs/jsonWebToken";
+import { DbConnection } from "../modules/connect";
+const { JWT_SECRET } = jsonWebToken;
 
-type ResponseType = { count: number }[]
+type ResponseType = { count: number }[];
 
-export const authorizer = (
-    req: ReqConnectionUndefined,
-    res: Response,
-    next: NextFunction
+export const authorizer = async (
+  req: ReqConnectionUndefined,
+  res: Response,
+  next: NextFunction
 ) => {
-    const { authorization: bearerToken } = req.headers
-    const { mysqlConnection: connection } = req
-    const token = bearerToken?.replace('Bearer ', '') || ''
-
-    let payload: any = {}
-    try {
-        payload = JWT.verify(token, JWT_SECRET)
-    } catch (e) {
-        throw new Error('E0005')
-    }
-    const { email, iat } = payload
-    connection
-        ?.run(`SELECT COUNT(*) AS count FROM user_info WHERE email=?`, [email])
-        .then((response: ResponseType) => {
-            const { count } = response[0]
-            if (count >= 1) {
-                next()
-            } else {
-                throw new Error('E0005')
-            }
-        })
-        .catch((e: Error) => {
-            next(e)
-        })
-}
+  let payload: any = {};
+  const request = req as ReqConnection;
+  try {
+    const { authorization: bearerToken } = req.headers;
+    const { mysqlConnection: connection } = request;
+    const token = bearerToken?.replace("Bearer ", "") || "";
+    payload = JWT.verify(token, JWT_SECRET);
+    const { email, iat } = payload;
+    await connection
+      .run(`SELECT COUNT(*) AS count FROM user_info WHERE email=?`, [email])
+      .then((response: ResponseType) => {
+        const { count } = response[0];
+        if (count >= 1) {
+          next();
+        } else {
+          throw new Error("E0005");
+        }
+      })
+      .catch((e: Error) => {
+        next(e);
+      });
+  } catch (e) {
+    throw new Error("E0005");
+  }
+};
