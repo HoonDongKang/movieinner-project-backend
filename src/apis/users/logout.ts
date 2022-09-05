e1d0a84d5bf357302e59a81c1b625fb177685772
HoonDongKang <d159123@naver.com>
2022-09-05 11:55:06 +0900

    logout

--- src/apis/users/logout.ts
+++ src/apis/users/logout.ts
@@ -1,3 +1,12 @@
 import { DbConnection } from '../../modules/connect'
 
-const logout = async (params: any, connection: DbConnection) => {}
+const logout = async (params: any, connection: DbConnection) => {
+    const {refreshToken}=params
+    try{
+        const response=await connection.run(`SELECT COUNT(*) as count FROM user_token WHERE refresh_token=?`,[refreshToken])
+        const {count}=response[0]
+        if (count===0)throw 'E0008'
+    } catch(e:any){
+        throw new Error(e)
+    }
+}
