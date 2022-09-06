94a36c39e9f3832b929594168968d3e757ff56ff
HoonDongKang <d159123@naver.com>
2022-09-06 10:41:34 +0900

    signup

--- src/apis/users/signup.ts
+++ src/apis/users/signup.ts
@@ -1,40 +1,46 @@
 //회원가입 API
-import bcrypt from 'bcrypt'
-import { DbConnection } from '../../modules/connect'
-import { paramsErrorHandler } from '../../modules/paramsError'
+import bcrypt from "bcrypt";
+import { DbConnection } from "../../modules/connect";
+import { paramsErrorHandler } from "../../modules/paramsError";
 
 //회원가입
 
 interface SignupParamsType {
-    email: string
-    password: string
-    image_URL: string
-    name: string
-    gender: string
-    birth: string
-    nickname: string
+  email: string;
+  password: string;
+  image_URL: string;
+  name: string;
+  gender: string;
+  birth: string;
+  nickname: string;
 }
 
 const signup = async (params: SignupParamsType, connection: DbConnection) => {
-    try {
-        const { email, password, image_URL, name, gender, birth, nickname } =
-            params
-        const salt = await bcrypt.genSalt(10)
-        if (!password) throw 'E0003'
-        const hashedPw = await bcrypt.hash(password, salt)
-        await connection.run(
-            `INSERT INTO user_info(email,password,image_URL,name,gender,birth,nickname) VALUES(?,?,?,?,?,?,?)`,
-            [email, hashedPw, image_URL, name, gender, birth, nickname]
-        )
-        return {
-            status: 201,
-            data: { success: true },
-        }
-    } catch (e: any) {
-        paramsErrorHandler(e)
+  try {
+    const { email, password, image_URL, name, gender, birth, nickname } =
+      params;
+    const salt = await bcrypt.genSalt(10);
+    if (!password) {
+      await connection.run(
+        `INSERT INTO user_info(email,image_URL,name,gender,birth,nickname) VALUES(?,?,?,?,?,?)`,
+        [email, image_URL, name, gender, birth, nickname]
+      );
+    } else {
+      const hashedPw = await bcrypt.hash(password, salt);
+      await connection.run(
+        `INSERT INTO user_info(email,password,image_URL,name,gender,birth,nickname) VALUES(?,?,?,?,?,?,?)`,
+        [email, hashedPw, image_URL, name, gender, birth, nickname]
+      );
     }
-}
+  } catch (e: any) {
+    paramsErrorHandler(e);
+  }
+  return {
+    status: 201,
+    data: { success: true },
+  };
+};
 
 export default {
-    signup,
-}
+  signup,
+};
