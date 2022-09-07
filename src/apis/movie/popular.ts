c74908e8dde92e7f39569474d7a3f4f5a74f0212
HoonDongKang <d159123@naver.com>
2022-09-07 13:21:45 +0900

    popular movie list

--- src/apis/movie/popular.ts
+++ src/apis/movie/popular.ts
@@ -1,8 +1,12 @@
 import { DbConnection } from "../../modules/connect";
+import axios from 'axios'
+import TMDB from '../../configs/tmdb'
+
+const {TMDB_API_KEY,TMDB_IMAGE_URL}=TMDB
 
 const getPopularMovies=async(params:any,connection:DbConnection)=>{
     try{
-        
+        //popular movie page
     } catch(e:any){
         throw new Error(e)
     }
