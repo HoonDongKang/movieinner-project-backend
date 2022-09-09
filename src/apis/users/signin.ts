35ecace7a12d868d5de5fc9a7680be772f77a747
HoonDongKang <d159123@naver.com>
2022-09-09 18:44:57 +0900

    login boolean

--- src/apis/users/signin.ts
+++ src/apis/users/signin.ts
@@ -7,25 +7,37 @@ const signin = async (
     params: { email: string; password: string },
     connection: DbConnection
 ) => {
-    let isEqual = false
     try {
         const { email, password } = params
         const response = await connection.run(
             `SELECT password FROM user_info WHERE email=?`,
             [email]
         )
-        if (!response[0]) {
-            throw 'E0006'
-        }
-        const { password: hashedpassword } = response[0]
-        isEqual = await bcrypt.compare(password, hashedpassword)
-        if (!isEqual) {
-            throw 'E0003'
+        if (response[0]) {
+            const { password: hashedpassword } = response[0]
+            const isPasswordEqual = await bcrypt.compare(
+                password,
+                hashedpassword
+            )
+            return {
+                status: 200,
+                data: {
+                    isEmailEqual: true,
+                    isPasswordEqual: isPasswordEqual,
+                },
+            }
+        } else {
+            return {
+                status: 200,
+                data: {
+                    isEmailEqual: false,
+                    isPasswordEqual: false,
+                },
+            }
         }
     } catch (e: any) {
         paramsErrorHandler(e)
     }
-    return { status: 200, data: { login: isEqual } }
 }
 
 export default { signin }
