1e611642ff3dca332592635d6d27067ab106845e
HoonDongKang <d159123@naver.com>
2022-09-10 22:43:24 +0900

    movie api

--- src/apis/movie/popular.ts
+++ src/apis/movie/popular.ts
@@ -1,13 +1,15 @@
-import { DbConnection } from "../../modules/connect";
+import { DbConnection } from '../../modules/connect'
 import axios from 'axios'
 import TMDB from '../../configs/tmdb'
 
-const {TMDB_API_KEY,TMDB_IMAGE_URL}=TMDB
+const { TMDB_API_KEY, TMDB_IMAGE_URL } = TMDB
 
-const getPopularMovies=async(params:any,connection:DbConnection)=>{
-    try{
-        //popular movie page
-    } catch(e:any){
+const getPopularMovies = async (params: any, connection: DbConnection) => {
+    try {
+        const response = axios.post(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=ko&page=1`
+        )
+    } catch (e: any) {
         throw new Error(e)
     }
 }
