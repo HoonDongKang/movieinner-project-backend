73824fba08f554c22916d53d6a07f8c34f189ee7
HoonDongKang <d159123@naver.com>
2022-09-12 21:11:15 +0900

    playing movie api

---
+++ src/apis/movie/playing.ts
@@ -0,0 +1,43 @@
+import { DbConnection } from '../../modules/connect'
+import axios from 'axios'
+import TMDB from '../../configs/tmdb'
+import { paramsErrorHandler } from './../../modules/paramsError'
+
+const { TMDB_API_KEY, TMDB_IMAGE_URL } = TMDB
+
+const upcomingMovies = async (params: any, connection: DbConnection) => {
+    const { page } = params
+    try {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/movie/upcoming?api_key=${TMDB_API_KEY}&language=ko&page=${page}`
+        )
+        const movieDetails = response.data
+        return {
+            status: 201,
+            data: movieDetails,
+        }
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+}
+
+const nowPlayingMovies = async (params: any, connection: DbConnection) => {
+    const { page } = params
+    try {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API_KEY}&language=ko&page=${page}`
+        )
+        const movieDetails = response.data
+        return {
+            status: 201,
+            data: movieDetails,
+        }
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+}
+
+export default {
+    upcomingMovies,
+    nowPlayingMovies,
+}
