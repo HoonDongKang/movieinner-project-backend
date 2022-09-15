994ba6859f0fa2780d5888aa87ceeafa2d670cc5
HoonDongKang <d159123@naver.com>
2022-09-15 23:45:55 +0900

    release date

--- src/apis/movie/theme.ts
+++ src/apis/movie/theme.ts
@@ -1,51 +1,51 @@
-import { DbConnection } from "../../modules/connect";
-import { paramsErrorHandler } from "../../modules/paramsError";
+import { DbConnection } from '../../modules/connect'
+import { paramsErrorHandler } from '../../modules/paramsError'
 
 const getAllThemes = async (params: any, connection: DbConnection) => {
-  const response = await connection.run(`SELECT * FROM movie_theme`);
-  return {
-    status: 201,
-    data: response,
-  };
-};
+    const response = await connection.run(`SELECT * FROM movie_theme`)
+    return {
+        status: 201,
+        data: response,
+    }
+}
 
 const getMoviesFromTheme = async (params: any, connection: DbConnection) => {
-  const { name } = params; //path
-  let response = {};
-  try {
-    response = await connection.run(
-      `SELECT * FROM movie_theme WHERE theme_name=?`,
-      [name]
-    );
-  } catch (e: any) {
-    paramsErrorHandler(e);
-  }
-  return {
-    status: 201,
-    data: response,
-  };
-};
+    const { name } = params //path
+    let response = {}
+    try {
+        response = await connection.run(
+            `SELECT * FROM movie_theme WHERE theme_name=?`,
+            [name]
+        )
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+    return {
+        status: 201,
+        data: response,
+    }
+}
 
 const insertMoviesinTheme = async (params: any, connection: DbConnection) => {
-  const { themeName, movieId, movieName } = params;
-  try {
-    await connection.run(
-      `INSERT INTO movie_theme(theme_name,movie_id,movie_name) VALUES (?,?,?)`,
-      [themeName, movieId, movieName]
-    );
-  } catch (e: any) {
-    paramsErrorHandler(e);
-  }
-  return {
-    status: 201,
-    data: {
-      success: true,
-    },
-  };
-};
+    const { themeName, movieId, movieName, releaseDate } = params
+    try {
+        await connection.run(
+            `INSERT INTO movie_theme(theme_name,movie_id,movie_name,release_date) VALUES (?,?,?,?)`,
+            [themeName, movieId, movieName, releaseDate]
+        )
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+    return {
+        status: 201,
+        data: {
+            success: true,
+        },
+    }
+}
 
 export default {
-  getAllThemes,
-  getMoviesFromTheme,
-  insertMoviesinTheme,
-};
+    getAllThemes,
+    getMoviesFromTheme,
+    insertMoviesinTheme,
+}
