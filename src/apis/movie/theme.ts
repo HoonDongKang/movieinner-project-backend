fe407225c5cdf8438aeadc8a2d8855d370b49514
HoonDongKang <d159123@naver.com>
2022-09-16 18:53:31 +0900

    get details from tmdb

--- src/apis/movie/theme.ts
+++ src/apis/movie/theme.ts
@@ -1,51 +1,54 @@
-import { DbConnection } from '../../modules/connect'
-import { paramsErrorHandler } from '../../modules/paramsError'
+import { DbConnection } from "../../modules/connect";
+import TMDB from '../../configs/tmdb'
+import { paramsErrorHandler } from './../../modules/paramsError'
+import axios from "axios";
+
+const { TMDB_API_KEY, TMDB_IMAGE_URL } = TMDB
 
 const getAllThemes = async (params: any, connection: DbConnection) => {
-    const response = await connection.run(`SELECT * FROM movie_theme`)
-    return {
-        status: 201,
-        data: response,
-    }
-}
+  const response = await connection.run(`SELECT * FROM movie_theme`);
+  return {
+    status: 201,
+    data: response,
+  };
+};
 
 const getMoviesFromTheme = async (params: any, connection: DbConnection) => {
-    const { name } = params //path
-    let response = {}
-    try {
-        response = await connection.run(
-            `SELECT * FROM movie_theme WHERE theme_name=?`,
-            [name]
-        )
-    } catch (e: any) {
-        paramsErrorHandler(e)
-    }
-    return {
-        status: 201,
-        data: response,
-    }
-}
+  const { name } = params; //path
+  let response = {};
+  try {
+    response = await connection.run(
+      `SELECT * FROM movie_theme WHERE theme_name=?`,
+      [name]
+    );
+  } catch (e: any) {
+    paramsErrorHandler(e);
+  }
+  return {
+    status: 201,
+    data: response,
+  };
+};
 
 const insertMoviesinTheme = async (params: any, connection: DbConnection) => {
-    const { themeName, movieId, movieName, releaseDate } = params
-    try {
-        await connection.run(
-            `INSERT INTO movie_theme(theme_name,movie_id,movie_name,release_date) VALUES (?,?,?,?)`,
-            [themeName, movieId, movieName, releaseDate]
-        )
-    } catch (e: any) {
-        paramsErrorHandler(e)
-    }
-    return {
-        status: 201,
-        data: {
-            success: true,
-        },
-    }
-}
+  const { movieId } = params;
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${TMDB_API_KEY}&language=ko`
+  )
+  } catch (e: any) {
+    paramsErrorHandler(e);
+  }
+  return {
+    status: 201,
+    data: {
+      success: true,
+    },
+  };
+};
 
 export default {
-    getAllThemes,
-    getMoviesFromTheme,
-    insertMoviesinTheme,
-}
+  getAllThemes,
+  getMoviesFromTheme,
+  insertMoviesinTheme,
+};
