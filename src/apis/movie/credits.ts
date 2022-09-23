658e04c61add2e40b689b2be4c1b0fa81bd8be0c
HoonDongKang <d159123@naver.com>
2022-09-23 22:54:43 +0900

    credits

--- src/apis/movie/credits.ts
+++ src/apis/movie/credits.ts
@@ -1,23 +1,26 @@
 import { DbConnection } from '../../modules/connect'
 import axios from 'axios'
 import TMDB from '../../configs/tmdb'
-import { paramsErrorHandler } from './../../modules/paramsError'
+import { tmdbErrorHandler } from './../../modules/paramsError'
 
 const { TMDB_API_KEY, TMDB_IMAGE_URL } = TMDB
 
 const getCredits = async (params: any, conncetion: DbConnection) => {
     const { movieId } = params
+    const movieCredits: any[] = []
     try {
         const response = await axios.get(
             `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${TMDB_API_KEY}&language=ko`
         )
-        const movieDetails = response.data.cast
+        for (let i = 0; i < 7; i++) {
+            movieCredits.push(response.data.cast[i])
+        }
         return {
             status: 201,
-            data: movieDetails,
+            data: movieCredits,
         }
     } catch (e: any) {
-        paramsErrorHandler(e)
+        tmdbErrorHandler(e)
     }
 }
 
