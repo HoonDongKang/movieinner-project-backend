88bef25b67489027eccbe70907e2bfe6ee77c1db
HoonDongKang <d159123@naver.com>
2022-09-27 21:32:17 +0900

    like

--- src/apis/movie/like.ts
+++ src/apis/movie/like.ts
@@ -1,4 +1,13 @@
 //찜한 영화
+import { DbConnection } from './../../modules/connect'
+
+const likedMovie = async (params: any, connection: DbConnection) => {
+    const { type, nickname, movieId } = params
+}
+
 //찜한 테마
 //최근 감상 작품
 //DB 저장
+export default {
+    likedMovie,
+}
