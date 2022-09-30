3ce43b81421d0b731a945dd25effb5c08a5fdbc0
HoonDongKang <d159123@naver.com>
2022-10-01 02:46:54 +0900

    structure

---
+++ src/apis/community/contents.ts
@@ -0,0 +1,13 @@
+import { DbConnection } from '../../modules/connect'
+
+const writeContents = async (params: any, connection: DbConnection) => {}
+
+const modifyContents = async (params: any, connection: DbConnection) => {}
+
+const deleteContents = async (params: any, connection: DbConnection) => {}
+
+export default {
+    writeContents,
+    modifyContents,
+    deleteContents,
+}
