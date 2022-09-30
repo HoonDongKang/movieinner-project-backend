f018216dac6a586a8f5ac74dfea2e68ee5924439
HoonDongKang <d159123@naver.com>
2022-09-30 22:22:47 +0900

    document

--- src/apis/movie/like.ts
+++ src/apis/movie/like.ts
@@ -1,94 +1,91 @@
-import {
-  paramsErrorHandler,
-  tmdbErrorHandler,
-} from "../../modules/paramsError";
-import { DbConnection } from "./../../modules/connect";
+import { paramsErrorHandler, tmdbErrorHandler } from '../../modules/paramsError'
+import { DbConnection } from './../../modules/connect'
 //찜하기 확인
 const checkLiked = async (params: any, connection: DbConnection) => {
-  const { type, nickname, movieId, name } = params;
-  let isExisted = false;
-  try {
-    if (type === "movie") {
-      const response = await connection.run(
-        `SELECT COUNT(*) AS count FROM liked WHERE type=? AND nickname=? AND movie_id=?`,
-        [type, nickname, movieId]
-      );
-      const { count } = response[0];
-      if (count > 0) {
-        isExisted = true;
-      }
-    } else if (type === "theme") {
-      const response = await connection.run(
-        `SELECT COUNT(*) AS count FROM liked WHERE type=? AND nickname=? AND name=?`,
-        [type, nickname, name]
-      );
-      const { count } = response[0];
-      if (count > 0) {
-        isExisted = true;
-      }
-    } else {
-      throw "E0001";
+    const { type, nickname, movieId, name } = params
+    let isExisted = false
+    try {
+        if (type === 'movie') {
+            const response = await connection.run(
+                `SELECT COUNT(*) AS count FROM liked WHERE type=? AND nickname=? AND movie_id=?`,
+                [type, nickname, movieId]
+            )
+            const { count } = response[0]
+            if (count > 0) {
+                isExisted = true
+            }
+        } else if (type === 'theme') {
+            const response = await connection.run(
+                `SELECT COUNT(*) AS count FROM liked WHERE type=? AND nickname=? AND name=?`,
+                [type, nickname, name]
+            )
+            const { count } = response[0]
+            if (count > 0) {
+                isExisted = true
+            }
+        } else {
+            throw 'E0001'
+        }
+    } catch (e: any) {
+        paramsErrorHandler(e)
     }
-  } catch (e: any) {
-    paramsErrorHandler(e);
-  }
-  return {
-    status: 201,
-    data: {
-      isExisted,
-    },
-  };
-};
+    return {
+        status: 201,
+        data: {
+            isExisted,
+        },
+    }
+}
 //찜한 영화
 const likedMovie = async (params: any, connection: DbConnection) => {
-  const { type, nickname, movieId, name, poster_path, backdrop_path } = params;
-  try {
-    if (type === "movie") {
-      await connection.run(
-        `INSERT INTO liked(type,nickname,movie_id,name,poster_path,backdrop_path) VALUES (?,?,?,?,?,?)`,
-        [type, nickname, movieId, name, poster_path, backdrop_path]
-      );
-    } else if (type === "theme") {
-      await connection.run(
-        `INSERT INTO liked(type,nickname,name) VALUES (?,?,?)`,
-        [type, nickname, name]
-      );
-    } else {
-      throw "E0001";
-    }
+    const { type, nickname, movieId, name, poster_path, backdrop_path } = params
+    try {
+        if (type === 'movie') {
+            await connection.run(
+                `INSERT INTO liked(type,nickname,movie_id,name,poster_path,backdrop_path) VALUES (?,?,?,?,?,?)`,
+                [type, nickname, movieId, name, poster_path, backdrop_path]
+            )
+        } else if (type === 'theme') {
+            await connection.run(
+                `INSERT INTO liked(type,nickname,name) VALUES (?,?,?)`,
+                [type, nickname, name]
+            )
+        } else {
+            throw 'E0001'
+        }
 
-    return {
-      status: 201,
-      data: {
-        success: true,
-      },
-    };
-  } catch (e: any) {
-    paramsErrorHandler(e);
-  }
-};
+        return {
+            status: 201,
+            data: {
+                success: true,
+            },
+        }
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+}
 
 //찜하기 취소
 const deleteLike = async (params: any, connection: DbConnection) => {
-  const { type, nickname, name } = params;
-  try {
-    await connection.run(
-      `DELETE FROM liked WHERE type=? AND nickname=? AND name=?`,
-      [type, nickname, name]
-    );
-    return {
-      status: 201,
-      data: {
-        success: true,
-      },
-    };
-  } catch (e: any) {
-    paramsErrorHandler(e);
-  }
-};
+    const { type, nickname, name } = params
+    try {
+        await connection.run(
+            `DELETE FROM liked WHERE type=? AND nickname=? AND name=?`,
+            [type, nickname, name]
+        )
+        return {
+            status: 201,
+            data: {
+                success: true,
+            },
+        }
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+}
 
 export default {
-  checkLiked,
-  likedMovie,
-  deleteLike,
-};
+    checkLiked,
+    likedMovie,
+    deleteLike,
+}
