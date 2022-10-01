dfe9f4fe26f1b0ee2738a719f6804becc7848c23
HoonDongKang <d159123@naver.com>
2022-10-01 12:09:35 +0900

    params

--- src/apis/community/contents.ts
+++ src/apis/community/contents.ts
@@ -1,10 +1,16 @@
 import { DbConnection } from '../../modules/connect'
 
-const writeContents = async (params: any, connection: DbConnection) => {}
+const writeContents = async (params: any, connection: DbConnection) => {
+    const { nickname, content, file } = params
+}
 
-const modifyContents = async (params: any, connection: DbConnection) => {}
+const modifyContents = async (params: any, connection: DbConnection) => {
+    const { nickname, content, file } = params
+}
 
-const deleteContents = async (params: any, connection: DbConnection) => {}
+const deleteContents = async (params: any, connection: DbConnection) => {
+    const { nickname, content, file } = params
+}
 
 export default {
     writeContents,
