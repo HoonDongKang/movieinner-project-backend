caf665262ced565a5b1055735d57bea2c8a6b0ad
HoonDongKang <d159123@naver.com>
2022-10-02 23:52:44 +0900

    community

--- src/apis/community/contents.ts
+++ src/apis/community/contents.ts
@@ -1,4 +1,5 @@
 import { DbConnection } from '../../modules/connect'
+import { paramsErrorHandler } from './../../modules/paramsError'
 
 const writeContents = async (params: any, connection: DbConnection) => {
     const { nickname, title, content, file } = params
@@ -7,20 +8,27 @@ const writeContents = async (params: any, connection: DbConnection) => {
             `INSERT INTO community(nickname,title,content,file) VALUES (?,?,?,?)`,
             [nickname, title, content, file]
         )
-    } catch (e: any) {}
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
 }
 
 const modifyContents = async (params: any, connection: DbConnection) => {
     const { nickname, title, content, file } = params
     try {
-        await connection.run()
+        await connection.run(
+            `UPDATE community(title,content,file) SET (?,?,?) WHERE nickname=?`,
+            [title, content, file, nickname]
+        )
     } catch (e: any) {}
 }
 
 const deleteContents = async (params: any, connection: DbConnection) => {
-    const { nickname, title, content, file } = params
+    const { nickname } = params
     try {
-        await connection.run()
+        await connection.run(`DELETE FROM community WHERE nickname=?`, [
+            nickname,
+        ])
     } catch (e: any) {}
 }
 
