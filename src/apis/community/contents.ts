3d9ec1f16dfe62c95b6bc61ad22eb0d7514d1c6c
HoonDongKang <d159123@naver.com>
2022-10-02 23:32:31 +0900

    insert

--- src/apis/community/contents.ts
+++ src/apis/community/contents.ts
@@ -1,21 +1,24 @@
 import { DbConnection } from '../../modules/connect'
 
 const writeContents = async (params: any, connection: DbConnection) => {
-    const { nickname, content, file } = params
+    const { nickname, title, content, file } = params
     try {
-        await connection.run()
+        await connection.run(
+            `INSERT INTO community(nickname,title,content,file) VALUES (?,?,?,?)`,
+            [nickname, title, content, file]
+        )
     } catch (e: any) {}
 }
 
 const modifyContents = async (params: any, connection: DbConnection) => {
-    const { nickname, content, file } = params
+    const { nickname, title, content, file } = params
     try {
         await connection.run()
     } catch (e: any) {}
 }
 
 const deleteContents = async (params: any, connection: DbConnection) => {
-    const { nickname, content, file } = params
+    const { nickname, title, content, file } = params
     try {
         await connection.run()
     } catch (e: any) {}
