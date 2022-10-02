c49541ce8d2d380be0dca7be9fc0ab345c2cfdd2
HoonDongKang <d159123@naver.com>
2022-10-02 23:30:05 +0900

    structure

--- src/apis/community/contents.ts
+++ src/apis/community/contents.ts
@@ -2,14 +2,23 @@ import { DbConnection } from '../../modules/connect'
 
 const writeContents = async (params: any, connection: DbConnection) => {
     const { nickname, content, file } = params
+    try {
+        await connection.run()
+    } catch (e: any) {}
 }
 
 const modifyContents = async (params: any, connection: DbConnection) => {
     const { nickname, content, file } = params
+    try {
+        await connection.run()
+    } catch (e: any) {}
 }
 
 const deleteContents = async (params: any, connection: DbConnection) => {
     const { nickname, content, file } = params
+    try {
+        await connection.run()
+    } catch (e: any) {}
 }
 
 export default {
