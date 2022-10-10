8ee145fc91ec94db09151badb0e2f9d76bf3a566
HoonDongKang <d159123@naver.com>
2022-10-10 13:38:39 +0900

    structure

--- src/apis/community/commtents.ts
+++ src/apis/community/commtents.ts
@@ -1,4 +1,17 @@
-// db 구축
-// idx 별 comments
-// user 별 comments
-// comments 의 comments
+import { DbConnection } from './../../modules/connect'
+
+const writeComment = async (params: any, connection: DbConnection) => {}
+const modifyComment = async (params: any, connection: DbConnection) => {}
+const deleteComment = async (params: any, connection: DbConnection) => {}
+
+const getIdxComments = async (params: any, connection: DbConnection) => {}
+
+const getUserComments = async (params: any, connection: DbConnection) => {}
+
+export default {
+    writeComment,
+    modifyComment,
+    deleteComment,
+    getIdxComments,
+    getUserComments,
+}
