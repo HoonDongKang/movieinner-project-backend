c2451a4005b2f85768bc1372ec7a04713f249310
HoonDongKang <d159123@naver.com>
2022-10-11 23:43:37 +0900

    write comment api

--- src/apis/community/commtents.ts
+++ src/apis/community/commtents.ts
@@ -1,6 +1,17 @@
 import { DbConnection } from './../../modules/connect'
+import { paramsErrorHandler } from './../../modules/paramsError'
 
-const writeComment = async (params: any, connection: DbConnection) => {}
+const writeComment = async (params: any, connection: DbConnection) => {
+    const { contentIdx, nickname, comment, commented_at } = params
+    try {
+        await connection.run(
+            `INSERT INTO comments(content_idx,nickname,comment,commented_at) VALUES (?,?,?,?)`,
+            [contentIdx, nickname, comment, commented_at]
+        )
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+}
 const modifyComment = async (params: any, connection: DbConnection) => {}
 const deleteComment = async (params: any, connection: DbConnection) => {}
 
