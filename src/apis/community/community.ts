5823338eaf756ff6462113424d12434ce238cfa3
HoonDongKang <d159123@naver.com>
2022-10-18 21:06:11 +0900

    getContents module

--- src/apis/community/community.ts
+++ src/apis/community/community.ts
@@ -1,48 +1,15 @@
 import { DbConnection } from './../../modules/connect'
 import { paramsErrorHandler } from './../../modules/paramsError'
-//
+import { getContentsPerPages } from './../../modules/getContents'
+
 const getAllContents = async (params: any, connection: DbConnection) => {
     const { page } = params
-    let contents: any = {}
     try {
         const response: any = await connection.run(
             `SELECT idx,nickname,title,content,file FROM community`,
             []
         )
-        //게시글 번호
-        for (let i = 0; i < response.length; i++) {
-            response[i]['number'] = i + 1
-        }
-        // 게시글 수
-        let contentsNumber: number = response.length - 1
-        // 페이지 당 게시글 표시 수
-        const contentsNumberInPage: number = 10
-        // 총 페이지 수
-        const pageNumber = Math.trunc(contentsNumber / contentsNumberInPage)
-        const totalPage =
-            contentsNumber % contentsNumberInPage === 0
-                ? pageNumber
-                : pageNumber + 1
-
-        for (let i = 1; i < totalPage + 1; i++) {
-            contents[i] = []
-            for (
-                let j = contentsNumber;
-                j > contentsNumber - contentsNumberInPage;
-                j--
-            ) {
-                console.log(j)
-                contents[i].push(response[j])
-            }
-            contentsNumber -= contentsNumberInPage
-        }
-        const responseContents = contents[page]
-
-        for (let i = 0; i < responseContents.length; i++) {
-            if (!responseContents[i]) {
-                responseContents[i] = {}
-            }
-        }
+        const responseContents = getContentsPerPages(response, 10, page)
         return {
             status: 200,
 
---
+++ src/modules/getContents.ts
@@ -0,0 +1,41 @@
+export const getContentsPerPages = (
+    array: any[], // contents array
+    contentsNumberPerPage: number, // a number of contents in a page
+    page: string // a page that to be responsed
+) => {
+    let contents: any = {}
+    //게시글 번호
+    for (let i = 0; i < array.length; i++) {
+        array[i]['number'] = i + 1
+    }
+    // 게시글 수
+    let contentsNumber: number = array.length - 1
+
+    // 총 페이지 수
+    const pageNumber = Math.trunc(contentsNumber / contentsNumberPerPage)
+    const totalPage =
+        contentsNumber % contentsNumberPerPage === 0
+            ? pageNumber
+            : pageNumber + 1
+
+    for (let i = 1; i < totalPage + 1; i++) {
+        contents[i] = []
+        for (
+            let j = contentsNumber;
+            j > contentsNumber - contentsNumberPerPage;
+            j--
+        ) {
+            console.log(j)
+            contents[i].push(array[j])
+        }
+        contentsNumber -= contentsNumberPerPage
+    }
+    const responseContents = contents[page]
+
+    for (let i = 0; i < responseContents.length; i++) {
+        if (!responseContents[i]) {
+            responseContents[i] = {}
+        }
+    }
+    return responseContents
+}
