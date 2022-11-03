cc180527ab18dcc047489f629563c48253cdf7de
HoonDongKang <d159123@naver.com>
2022-11-03 20:39:19 +0900

    get top hit contents api

--- src/apis/community/contents.ts
+++ src/apis/community/contents.ts
@@ -1,100 +1,100 @@
-import { DbConnection } from "../../modules/connect";
-import { paramsErrorHandler } from "./../../modules/paramsError";
+import { DbConnection } from '../../modules/connect'
+import { paramsErrorHandler } from './../../modules/paramsError'
 
 const writeContents = async (params: any, connection: DbConnection) => {
-  const { nickname, title, content, file } = params;
-  try {
-    const response = await connection.run(
-      `INSERT INTO community(nickname,title,content,file) VALUES (?,?,?,?)`,
-      [nickname, title, content, file]
-    );
-    const idx = response.insertId;
-    return {
-      status: 201,
-      data: {
-        idx,
-      },
-    };
-  } catch (e: any) {
-    paramsErrorHandler(e);
-  }
-};
+    const { nickname, title, content, file } = params
+    try {
+        const response = await connection.run(
+            `INSERT INTO community(nickname,title,content,file) VALUES (?,?,?,?)`,
+            [nickname, title, content, file]
+        )
+        const idx = response.insertId
+        return {
+            status: 201,
+            data: {
+                idx,
+            },
+        }
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+}
 
 const modifyContents = async (params: any, connection: DbConnection) => {
-  const { idx, title, content, file } = params;
-  try {
-    await connection.run(
-      `UPDATE community SET title=?,content=?,file=? WHERE idx=?`,
-      [title, content, file, idx]
-    );
-    return {
-      status: 201,
-      data: {
-        success: true,
-      },
-    };
-  } catch (e: any) {
-    paramsErrorHandler(e);
-  }
-};
+    const { idx, title, content, file } = params
+    try {
+        await connection.run(
+            `UPDATE community SET title=?,content=?,file=? WHERE idx=?`,
+            [title, content, file, idx]
+        )
+        return {
+            status: 201,
+            data: {
+                success: true,
+            },
+        }
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+}
 
 const deleteContents = async (params: any, connection: DbConnection) => {
-  const { idx } = params;
-  try {
-    await connection.run(`DELETE FROM community WHERE idx=?`, [idx]);
-    return {
-      status: 201,
-      data: {
-        success: true,
-      },
-    };
-  } catch (e: any) {
-    paramsErrorHandler(e);
-  }
-};
+    const { idx } = params
+    try {
+        await connection.run(`DELETE FROM community WHERE idx=?`, [idx])
+        return {
+            status: 201,
+            data: {
+                success: true,
+            },
+        }
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+}
 
 const increaseHit = async (params: any, connection: DbConnection) => {
-  const { idx } = params;
-  try {
-    const response = await connection.run(
-      `SELECT hit FROM community WHERE idx=?`,
-      [idx]
-    );
-    const { hit } = response[0];
-    const increaseHit = hit + 1;
-    await connection.run(`UPDATE community SET hit=? WHERE idx=?`, [
-      increaseHit,
-      idx,
-    ]);
-    return {
-      status: 201,
-      data: {
-        success: true,
-      },
-    };
-  } catch (e: any) {
-    paramsErrorHandler(e);
-  }
-};
+    const { idx } = params
+    try {
+        const response = await connection.run(
+            `SELECT hit FROM community WHERE idx=?`,
+            [idx]
+        )
+        const { hit } = response[0]
+        const increaseHit = hit + 1
+        await connection.run(`UPDATE community SET hit=? WHERE idx=?`, [
+            increaseHit,
+            idx,
+        ])
+        return {
+            status: 201,
+            data: {
+                success: true,
+            },
+        }
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+}
 
 const getTopHitContents = async (params: any, connection: DbConnection) => {
-  try {
-    const response = await connection.run(
-      `SELECT * FROM community ORDER BY hit DESC LIMIT 5`
-    );
-    return{
-        status:200,
-        data:response
+    try {
+        const response = await connection.run(
+            `SELECT idx,title,created_at FROM community ORDER BY hit DESC LIMIT 5`
+        )
+        return {
+            status: 200,
+            data: { top5Contents: response },
+        }
+    } catch (e: any) {
+        console.error(e)
     }
-  } catch (e: any) {
-    console.error(e);
-  }
-};
+}
 
 export default {
-  writeContents,
-  modifyContents,
-  deleteContents,
-  increaseHit,
-  getTopHitContents
-};
+    writeContents,
+    modifyContents,
+    deleteContents,
+    increaseHit,
+    getTopHitContents,
+}
