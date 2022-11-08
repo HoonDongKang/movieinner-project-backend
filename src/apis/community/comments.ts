5300bc247c440399a688673c670e5fa69c0ffe7a
HoonDongKang <d159123@naver.com>
2022-11-08 23:33:55 +0900

    write comment

--- src/apis/community/comments.ts
+++ src/apis/community/comments.ts
@@ -2,25 +2,40 @@ import { DbConnection } from '../../modules/connect'
 import { paramsErrorHandler } from '../../modules/paramsError'
 import { changeDbTimeForm } from './../../modules/changeTimeForm'
 import { getContentsPerPages } from './../../modules/getContents'
-//
+
+// idx 만 응답하고 idx를 통해 또 다른 api?
+// 한번에 입력값 출력?
+// 처음 요청 값에서 사용?
 const writeComment = async (params: any, connection: DbConnection) => {
-    const { contentIdx, nickname, comment, commentedAt } = params
+    const { contentIdx, nickname, comment, responseTo } = params
+    let response: any = []
+    let idx = 0
     try {
-        if (commentedAt === undefined) {
-            await connection.run(
+        if (responseTo === undefined) {
+            response = await connection.run(
                 `INSERT INTO comments(content_idx,nickname,comment) VALUES (?,?,?)`,
                 [contentIdx, nickname, comment]
             )
+            const { insertId } = response
+            idx = insertId
         } else {
-            await connection.run(
-                `INSERT INTO comments(content_idx,nickname,comment,commentedAt) VALUES (?,?,?,?)`,
-                [contentIdx, nickname, comment, commentedAt]
+            response = await connection.run(
+                `INSERT INTO comments(content_idx,nickname,comment,response_to) VALUES (?,?,?,?)`,
+                [contentIdx, nickname, comment, responseTo]
             )
+            const { insertId } = response
+            idx = insertId
         }
+        const getResponse = await connection.run(
+            `SELECT content_idx,nickname,comment,response_to,created_at FROM comments WHERE idx=?`,
+            [idx]
+        )
+        changeDbTimeForm(getResponse)
         return {
             status: 201,
             data: {
                 success: true,
+                comments: getResponse[0],
             },
         }
     } catch (e: any) {
@@ -63,7 +78,7 @@ const getIdxComments = async (params: any, connection: DbConnection) => {
     const { contentIdx, page } = params //path: contentIdx, query: page
     try {
         const response = await connection.run(
-            `SELECT idx, content_idx, nickname,comment, commented_at, created_at FROM comments WHERE content_idx=?`,
+            `SELECT idx, content_idx, nickname,comment, response_to, created_at FROM comments WHERE content_idx=?`,
             [contentIdx]
         )
         changeDbTimeForm(response)
@@ -87,7 +102,7 @@ const getUserComments = async (params: any, connection: DbConnection) => {
     const { nickname, page } = params //path: nickname, query: page
     try {
         const response = await connection.run(
-            `SELECT idx, content_idx, nickname,comment, commented_at, created_at FROM comments WHERE nickname=?`,
+            `SELECT idx, content_idx, nickname,comment, response_to, created_at FROM comments WHERE nickname=?`,
             [nickname]
         )
         changeDbTimeForm(response)
