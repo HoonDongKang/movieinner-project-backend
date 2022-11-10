75c43fe72325ce32273f42e0d54c065b9b162cbc
HoonDongKang <d159123@naver.com>
2022-11-10 21:47:29 +0900

    replace in title

--- src/apis/search/search.ts
+++ src/apis/search/search.ts
@@ -6,7 +6,7 @@ const contentsTitleSearch = async (params: any, connection: DbConnection) => {
     title = title.replace(/ /g, '') // g-> slash 안에 모든 문자 변경
     try {
         const response = await connection.run(
-            `SELECT idx,nickname,title,content,file,hit,created_at FROM community WHERE title LIKE ?`,
+            `SELECT idx,nickname,title,content,file,hit,created_at FROM community WHERE replace(title," ","") LIKE ?`,
             ['%' + title + '%']
         )
         return {
