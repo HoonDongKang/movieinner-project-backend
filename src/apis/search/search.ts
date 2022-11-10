201d269ae12eb46f98d1c6826fcc2385af5d1714
HoonDongKang <d159123@naver.com>
2022-11-10 21:48:53 +0900

    comments

--- src/apis/search/search.ts
+++ src/apis/search/search.ts
@@ -3,8 +3,10 @@ import { paramsErrorHandler } from './../../modules/paramsError'
 
 const contentsTitleSearch = async (params: any, connection: DbConnection) => {
     let { title } = params
+    // params 띄어쓰기 제거
     title = title.replace(/ /g, '') // g-> slash 안에 모든 문자 변경
     try {
+        // DB 값 띄어쓰기 제거
         const response = await connection.run(
             `SELECT idx,nickname,title,content,file,hit,created_at FROM community WHERE replace(title," ","") LIKE ?`,
             ['%' + title + '%']
