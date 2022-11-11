1c800a726e8c4904da799b8a0639c05dd5006992
HoonDongKang <d159123@naver.com>
2022-11-11 21:51:27 +0900

    clean

--- src/apis/search/search.ts
+++ src/apis/search/search.ts
@@ -4,58 +4,38 @@ import { changeDbTimeForm } from './../../modules/changeTimeForm'
 
 const contentsSearch = async (params: any, connection: DbConnection) => {
     let { type, search } = params //query params
+    let response: any[] = []
     // params 띄어쓰기 제거
     search = search.replace(/ /g, '') // g-> slash 안에 모든 문자 변경
     try {
         // DB 값 띄어쓰기 제거
         if (type === 'title') {
-            const response = await connection.run(
+            response = await connection.run(
                 `SELECT idx,nickname,title,content,file,hit,created_at FROM community WHERE replace(title," ","") LIKE ? ORDER BY hit DESC`,
                 ['%' + search + '%']
             )
-            changeDbTimeForm(response)
-            return {
-                status: 200,
-                data: {
-                    searchResult: response,
-                },
-            }
         } else if (type === 'content') {
-            const response = await connection.run(
+            response = await connection.run(
                 `SELECT idx,nickname,title,content,file,hit,created_at FROM community WHERE replace(content," ","") LIKE ? ORDER BY hit DESC`,
                 ['%' + search + '%']
             )
-            changeDbTimeForm(response)
-            return {
-                status: 200,
-                data: {
-                    searchResult: response,
-                },
-            }
         } else if (type === 'writer') {
-            const response = await connection.run(
+            response = await connection.run(
                 `SELECT idx,nickname,title,content,file,hit,created_at FROM community WHERE replace(nickname," ","") LIKE ? ORDER BY hit DESC`,
                 ['%' + search + '%']
             )
-            changeDbTimeForm(response)
-            return {
-                status: 200,
-                data: {
-                    searchResult: response,
-                },
-            }
         } else if (type === 'titleAndContent') {
-            const response = await connection.run(
+            response = await connection.run(
                 `SELECT idx,nickname,title,content,file,hit,created_at FROM community WHERE replace(content," ","") LIKE ? OR replace(title," ","") LIKE ? ORDER BY hit DESC`,
                 ['%' + search + '%', '%' + search + '%']
             )
-            changeDbTimeForm(response)
-            return {
-                status: 200,
-                data: {
-                    searchResult: response,
-                },
-            }
+        }
+        changeDbTimeForm(response)
+        return {
+            status: 200,
+            data: {
+                searchResult: response,
+            },
         }
     } catch (e: any) {
         paramsErrorHandler(e)
