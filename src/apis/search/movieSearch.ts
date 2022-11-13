fbba2f04493e33b712b87fd1f085f81bf80837a0
HoonDongKang <d159123@naver.com>
2022-11-13 21:03:36 +0900

    search movie

---
+++ src/apis/search/movieSearch.ts
@@ -0,0 +1,19 @@
+import { DbConnection } from './../../modules/connect'
+import TMDB from '../../configs/tmdb'
+
+const { TMDB_API_KEY } = TMDB
+
+const movieSearch = async (params: any, connection: DbConnection) => {
+    const { search, searchPage } = params
+    try {
+        const response = await connection.run(`
+        https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&language=ko&query=${search}&page=${searchPage}`)
+        const { page, results, total_pages, total_results } = response
+    } catch (e: any) {
+        console.error(e)
+    }
+}
+
+export default {
+    movieSearch,
+}
