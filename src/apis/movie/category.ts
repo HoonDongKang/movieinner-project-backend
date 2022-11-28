92b4899c6d787049fb4e926866d0292171141b6c
HoonDongKang <d159123@naver.com>
2022-11-29 00:35:04 +0900

    prettier

--- src/apis/movie/category.ts
+++ src/apis/movie/category.ts
@@ -1,49 +1,46 @@
 //카테고리별 영화 만들기
-import axios from "axios";
-import {
-  convertGenreIdtoName,
-  genreId,
-} from "../../modules/tmdbConvert";
-import { DbConnection } from "./../../modules/connect";
-import TMDB from "../../configs/tmdb";
+import axios from 'axios'
+import { convertGenreIdtoName, genreId } from '../../modules/tmdbConvert'
+import { DbConnection } from './../../modules/connect'
+import TMDB from '../../configs/tmdb'
 
-const { TMDB_API_KEY } = TMDB;
+const { TMDB_API_KEY } = TMDB
 
 const searchCategory = async (params: any, connection: DbConnection) => {
-  const { search, searchPage } = params; //query search:id
-  let resultArray: any[] = [];
-  try {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&with_genres=${search}&page=${searchPage}&language=ko`
-    );
-    const { page, results, total_pages, total_results } = response.data;
+    const { search, searchPage } = params //query search:id
+    let resultArray: any[] = []
+    try {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&with_genres=${search}&page=${searchPage}&language=ko`
+        )
+        const { page, results, total_pages, total_results } = response.data
 
-    results.forEach((objects:any) => {
-      resultArray.push({
-        id: objects.id,
-        genre: objects.genre_ids,
-        title: objects.title,
-        poster_path: objects.poster_path,
-        release_date: objects.release_date,
-        popularity: objects.popularity,
-      });
-    });
-    convertGenreIdtoName(resultArray)
+        results.forEach((objects: any) => {
+            resultArray.push({
+                id: objects.id,
+                genre: objects.genre_ids,
+                title: objects.title,
+                poster_path: objects.poster_path,
+                release_date: objects.release_date,
+                popularity: objects.popularity,
+            })
+        })
+        convertGenreIdtoName(resultArray)
 
-    return {
-      status: 200,
+        return {
+            status: 200,
             data: {
                 total_pages,
                 total_results,
                 current_page: page,
                 search: resultArray,
             },
-    };
-  } catch (e: any) {
-    console.error(e);
-  }
-};
+        }
+    } catch (e: any) {
+        console.error(e)
+    }
+}
 
 export default {
-  searchCategory,
-};
+    searchCategory,
+}
