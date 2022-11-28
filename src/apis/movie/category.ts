f86c7ce02f1867f49fa1f2e8a6b3f577635aaaef
HoonDongKang <d159123@naver.com>
2022-11-28 15:16:04 +0900

    category search api

--- src/apis/movie/category.ts
+++ src/apis/movie/category.ts
@@ -1,23 +1,20 @@
 //카테고리별 영화 만들기
-import { genreId } from '../../modules/tmdbConvert'
+import axios from 'axios'
+import { convertGenreIdtoName, genreId } from '../../modules/tmdbConvert'
 import { DbConnection } from './../../modules/connect'
+import TMDB from '../../configs/tmdb'
+
+const { TMDB_API_KEY } = TMDB
 
 const searchCategory = async (params: any, connection: DbConnection) => {
     const { search, searchPage } = params //query search:id
-    let genreName = ''
-    try {
-        for (const id of genreId) {
-            if (Number(search) === id.id) {
-                genreName = id.name
-                break
-            } else {
-                genreName = 'undefined genre'
-            }
-        }
 
+    try {
+        const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&with_genres=${search}&page${searchPage}&langauge=ko`)
+        convertGenreIdtoName(response)
         return {
             status: 200,
-            data: genreName,
+            data: response,
         }
     } catch (e: any) {
         console.error(e)
