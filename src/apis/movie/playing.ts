e2499986fcac572380aab0761c5b7e2f92f7c440
HoonDongKang <d159123@naver.com>
2022-12-02 23:56:58 +0900

    movie result change

--- src/apis/movie/playing.ts
+++ src/apis/movie/playing.ts
@@ -1,19 +1,45 @@
 import axios from 'axios'
 import TMDB from '../../configs/tmdb'
+import { convertGenreIdtoName } from '../../modules/tmdbConvert'
 import { tmdbErrorHandler } from './../../modules/paramsError'
+import {
+    MovieResultArrayType,
+    TmdbResultArrayType,
+} from './../search/movieSearch'
 
 const { TMDB_API_KEY } = TMDB
 
-const upcomingMovies = async (params: { page: string }, connection: never) => {
-    const { page } = params
+const upcomingMovies = async (
+    params: { searchPage: string },
+    connection: never
+) => {
+    const { searchPage } = params
+    let resultArray: Array<MovieResultArrayType> = []
     try {
         const response = await axios.get(
-            `https://api.themoviedb.org/3/movie/upcoming?api_key=${TMDB_API_KEY}&language=ko&page=${page}`
+            `https://api.themoviedb.org/3/movie/upcoming?api_key=${TMDB_API_KEY}&language=ko&page=${searchPage}`
         )
-        const movieDetails = response.data
+        const { page, results, total_pages, total_results } = response.data
+
+        results.forEach((objects: TmdbResultArrayType) => {
+            resultArray.push({
+                id: objects.id,
+                genre: objects.genre_ids,
+                title: objects.title,
+                poster_path: objects.poster_path,
+                release_date: objects.release_date,
+                popularity: objects.popularity,
+            })
+        })
+        convertGenreIdtoName(resultArray)
         return {
             status: 201,
-            data: movieDetails,
+            data: {
+                page,
+                total_pages,
+                total_results,
+                resultArray,
+            },
         }
     } catch (e: any) {
         tmdbErrorHandler(e)
@@ -21,18 +47,36 @@ const upcomingMovies = async (params: { page: string }, connection: never) => {
 }
 
 const nowPlayingMovies = async (
-    params: { page: string },
+    params: { searchPage: string },
     connection: never
 ) => {
-    const { page } = params
+    const { searchPage } = params
+    let resultArray: Array<MovieResultArrayType> = []
     try {
         const response = await axios.get(
-            `https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API_KEY}&language=ko&page=${page}`
+            `https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API_KEY}&language=ko&page=${searchPage}`
         )
-        const movieDetails = response.data
+        const { page, results, total_pages, total_results } = response.data
+
+        results.forEach((objects: TmdbResultArrayType) => {
+            resultArray.push({
+                id: objects.id,
+                genre: objects.genre_ids,
+                title: objects.title,
+                poster_path: objects.poster_path,
+                release_date: objects.release_date,
+                popularity: objects.popularity,
+            })
+        })
+        convertGenreIdtoName(resultArray)
         return {
             status: 201,
-            data: movieDetails,
+            data: {
+                page,
+                total_pages,
+                total_results,
+                resultArray,
+            },
         }
     } catch (e: any) {
         tmdbErrorHandler(e)
