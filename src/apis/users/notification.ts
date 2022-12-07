6ef1798d89ae3b3f650d249cfafafee16a959dbf
HoonDongKang <d159123@naver.com>
2022-12-07 23:28:54 +0900

    notification structure

--- src/apis/users/notification.ts
+++ src/apis/users/notification.ts
@@ -1,3 +1,26 @@
+import { DbConnection } from '../../modules/connect'
+
+const notification = async (params: any, connection: DbConnection) => {}
+
 // notfication
-// 댓글 달때마다 db 저장 기능 구현
+// 1) 내가 작성한 게시물에 댓글이 달렸다
+// ㄴ user_nickname: 나
+// ㄴ action_user_nicknmae: 댓글 단 사람
+// ㄴ not_type : content
+// ㄴ not_type_id : content_idx
+// ㄴ isChecked : 알림 확인했으면 0 안했으면 1
+
+// => 알림 숫자 : isChecked가 1인 개수
+// => 알림 내용 : content_idx 로 게시글 내용 불러와서 미리보기 OR comment_idx로 새로 달린 댓글 미리보기
+
+// 2) 내가 달은 댓글에 대댓글이 달렸다
+// ㄴ user_nickname: 나
+// ㄴ action_user_nicknmae: 대댓글 단 사람
+// ㄴ not_type : reply
+// ㄴ not_type_id : comments_idx
+// ㄴ isChecked : 알림 확인했으면 0 안했으면 1
+
+// => 알림 숫자 : isChecked가 1인 개수
+// => 알림 내용 : comments_idx 로 댓글 내용 불러와서 미리보기 OR 새로 달린 대댓글 내용 미리보기
+
 // s3 이미지 변경 api  multer
