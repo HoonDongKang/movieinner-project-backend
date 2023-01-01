0591014d9af76e8a282227cbbe30816927decf4f
HoonDongKang <d159123@naver.com>
2023-01-01 19:03:16 +0900

    UPDATE nicname to idx

--- src/apis/community/community.ts
+++ src/apis/community/community.ts
@@ -10,7 +10,7 @@ const getAllContents = async (
     const { page } = params
     try {
         const response = await connection.run(
-            `SELECT idx,nickname,title,content,file,hit,created_at FROM community`,
+            `SELECT CMT.idx,INFO.nickname,CMT.title,CMT.content,CMT.file,CMT.hit,CMT.created_at FROM community as CMT INNER JOIN user_info as INFO on CMT.user_idx=INFO.idx`,
             []
         )
         changeDbTimeForm(response)
@@ -31,14 +31,14 @@ const getAllContents = async (
 }
 
 const getUserContent = async (
-    params: { nickname: string },
+    params: { userIdx: string },
     connection: DbConnection
 ) => {
-    const { nickname } = params //query
+    const { userIdx } = params //query
     try {
         const response = await connection.run(
-            `SELECT idx, nickname,title,content,file,created_at FROM community WHERE nickname=?`,
-            [nickname]
+            `SELECT CMT.idx, INFO.nickname ,CMT.title,CMT.content,CMT.file,CMT.created_at FROM community AS CMT INNER JOIN user_info AS INFO ON CMT.user_idx = INFO.idx WHERE INFO.idx=?`,
+            [userIdx]
         )
         changeDbTimeForm(response)
         return {
@@ -55,10 +55,10 @@ const getIdxContent = async (
     params: { idx: string },
     connection: DbConnection
 ) => {
-    const { idx } = params //query
+    const { idx } = params //path
     try {
         const response = await connection.run(
-            `SELECT nickname,title,content,file,created_at FROM community WHERE idx =?`,
+            `SELECT INFO.nickname,CMT.title,CMT.content,CMT.file,CMT.created_at FROM community AS CMT INNER JOIN user_info AS INFO ON CMT.user_idx = INFO.idx WHERE CMT.idx=?`,
             [idx]
         )
         changeDbTimeForm(response)
