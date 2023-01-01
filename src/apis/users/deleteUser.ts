0daa4344781fec4c8008f64b698aa84771f88b65
HoonDongKang <d159123@naver.com>
2023-01-01 20:37:58 +0900

    UPDATE: delete

--- src/apis/users/deleteUser.ts
+++ src/apis/users/deleteUser.ts
@@ -15,21 +15,35 @@ const deleteUsers = async (params: never, connection: DbConnection) => {
 //나중에 delete 통합하는 법 찾기
 // notification table 삭제 추가하기
 const deleteUser = async (
-    params: { email: string; nickname: string },
+    params: { email: string; userIdx: string },
     connection: DbConnection
 ) => {
     try {
-        const { email, nickname } = params
+        const { email, userIdx } = params
         const response = await connection.run(
-            `DELETE FROM a,b,c,d,e,f USING user_info AS a LEFT JOIN user_token AS b ON a.email=b.email LEFT JOIN email_verify AS c ON a.email=c.email LEFT JOIN community AS d ON a.nickname=d.nickname LEFT JOIN liked AS e ON a.nickname=e.nickname LEFT JOIN comments AS f ON a.nickname=f.nickname WHERE a.email=? AND a.nickname=?`,
-            [email, nickname]
+            `DELETE FROM a,b,c,d,e,f USING user_info AS a LEFT JOIN user_token AS b ON a.email=b.email LEFT JOIN email_verify AS c ON a.email=c.email LEFT JOIN community AS d ON a.idx=d.user_idx LEFT JOIN liked AS e ON a.idx=e.user_idx LEFT JOIN comments AS f ON a.idx=f.user_idx WHERE a.email=? AND a.idx=?`,
+            [email, userIdx]
         )
         const { affectedRows } = response
         if (affectedRows === 0) throw 'E0008'
     } catch (e: any) {
         paramsErrorHandler(e)
     }
-    return { status: 201, data: { success: true } }
+    return {
+        status: 201,
+        data: { success: true },
+        cookie: {
+            name: 'refreshToken',
+            val: '',
+            options: {
+                httpOnly: true,
+                path: '/',
+                sameSite: 'lax',
+                expires: new Date(Date.now()),
+                // domain: 'http://localhost:3000',
+            },
+        },
+    }
 }
 
 export default {
