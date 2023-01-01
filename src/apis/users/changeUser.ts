344b4db9dd411833ad34db60800471acbef831c8
HoonDongKang <d159123@naver.com>
2023-01-01 19:39:49 +0900

    UPDATE: change params

--- src/apis/users/changeUser.ts
+++ src/apis/users/changeUser.ts
@@ -1,80 +1,67 @@
 // 유저 정보 CRUD API
-import { IsValidateName } from "../../configs/regExp";
-import { DbConnection } from "../../modules/connect";
-import { paramsErrorHandler } from "../../modules/paramsError";
+import { IsValidateName } from '../../configs/regExp'
+import { DbConnection } from '../../modules/connect'
+import { paramsErrorHandler } from '../../modules/paramsError'
 
 //특정 유저 비밀번호 변경
 const changeUserPassword = async (
-  params: { email: string; newPassword: string },
-  connection: DbConnection
+    params: { email: string; newPassword: string },
+    connection: DbConnection
 ) => {
-  try {
-    const { email, newPassword } = params;
-    await connection.run(`UPDATE user_info SET password=? WHERE email=?`, [
-      newPassword,
-      email,
-    ]);
-  } catch (e: any) {
-    paramsErrorHandler(e);
-  }
-  return { status: 200, data: { success: true } };
-};
+    try {
+        const { email, newPassword } = params
+        await connection.run(`UPDATE user_info SET password=? WHERE email=?`, [
+            newPassword,
+            email,
+        ])
+    } catch (e: any) {
+        paramsErrorHandler(e)
+    }
+    return { status: 200, data: { success: true } }
+}
 
 const changeUserNickname = async (
-  params: { nickname: string; email: string; newNickname: string },
-  connection: DbConnection
+    params: { userIdx: string; email: string; newNickname: string },
+    connection: DbConnection
 ) => {
-  //닉네임 유효성 검사 이후
-  const { nickname, email, newNickname } = params;
-  try {
-    const IsValid = IsValidateName(newNickname);
-    if (IsValid) {
-      const selectRes = await connection.run(
-        `SELECT COUNT(*) as count FROM user_info WHERE email=? AND nickname=?`,
-        [email, nickname]
-      );
-      const { count } = selectRes[0];
-      //이거 쿼리 하나로 끝내보기
-      if (count > 0) {
-        await connection.run(
-          `UPDATE user_info SET user_info.nickname=? WHERE user_info.nickname=?`,
-          [newNickname, nickname]
-        );
-        await connection.run(
-          `UPDATE liked SET liked.nickname=? WHERE liked.nickname=?`,
-          [newNickname, nickname]
-        );
-        await connection.run(
-          `UPDATE community SET community.nickname=? WHERE community.nickname=?`,
-          [newNickname, nickname]
-        );
-        await connection.run(
-          `UPDATE comments SET comments.nickname=? WHERE comments.nickname=?`,
-          [newNickname, nickname]
-        );
-      } else {
-        throw "E0008";
-      }
-      return {
-        status: 201,
-        data: {
-          success: true,
-        },
-      };
-    } else {
-      return {
-        status: 400,
-        data: {
-          success: false,
-        },
-      };
+    //닉네임 유효성 검사 이후
+    const { userIdx, newNickname } = params
+    try {
+        const IsValid = IsValidateName(newNickname)
+        if (IsValid) {
+            const selectRes = await connection.run(
+                `SELECT COUNT(*) as count FROM user_info WHERE idx=?`,
+                [userIdx]
+            )
+            const { count } = selectRes[0]
+            if (count > 0) {
+                await connection.run(
+                    `UPDATE user_info SET nickname=? WHERE idx=?`,
+                    [newNickname, userIdx]
+                )
+            } else {
+                throw 'E0008'
+            }
+            return {
+                status: 201,
+                data: {
+                    success: true,
+                },
+            }
+        } else {
+            return {
+                status: 400,
+                data: {
+                    success: false,
+                },
+            }
+        }
+    } catch (e: any) {
+        paramsErrorHandler(e)
     }
-  } catch (e: any) {
-    paramsErrorHandler(e);
-  }
-};
+}
 
 export default {
-  changeUserPassword,
-  changeUserNickname,
-};
+    changeUserPassword,
+    changeUserNickname,
+}
