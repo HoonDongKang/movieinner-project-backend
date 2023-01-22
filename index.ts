9e820e501ef2b8d988ff2a6b3810e1470cbe265e
HoonDongKang <d159123@naver.com>
2023-01-22 22:28:16 +0900

    update

--- index.ts
+++ index.ts
@@ -1,23 +1,24 @@
-import express from 'express'
-import cors from 'cors'
-import { useMysql } from './src/middlewares/dbConnect'
-import { registerAllApis } from './src/controllers'
-import { apiConfigs } from './src/configs/api'
-import router from './src/apis/image'
-import { errorHandler } from './src/middlewares/errorHandler'
-import cookieParser from 'cookie-parser'
+import express from "express"
+import cors from "cors"
+import { useMysql } from "./src/middlewares/dbConnect"
+import { registerAllApis } from "./src/controllers"
+import { apiConfigs } from "./src/configs/api"
+import router from "./src/apis/image"
+import { errorHandler } from "./src/middlewares/errorHandler"
+import cookieParser from "cookie-parser"
 
 const app = express()
 const PORT = 3714
 
-app.use(express.static('public'))
+app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors({ credentials: true, origin: true }))
 app.use(useMysql)
 app.use(cookieParser())
 
-app.use('/image', router)
+app.use("/image", router)
+console.log("image")
 registerAllApis(app, apiConfigs)
     .then(() => {
         app.use(errorHandler)
@@ -29,3 +30,4 @@ registerAllApis(app, apiConfigs)
         console.error(e)
         process.exit(-1)
     })
+console.log("all apis")
